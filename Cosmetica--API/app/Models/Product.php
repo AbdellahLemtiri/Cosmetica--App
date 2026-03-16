c45490f3aa041164580f884f1d95185ade837af4
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-03-16 12:12:29 +0000

    Add fillable properties to Product model and clean up User model's fillable array formatting

--- Cosmetica--API/app/Models/Product.php
+++ Cosmetica--API/app/Models/Product.php
@@ -11,6 +11,12 @@ class Product extends Model
     /** @use HasFactory<\Database\Factories\ProductFactory> */
     use HasFactory;
 
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'category_id',
+    ];
 
     public function category()
     {
