97b12c77a583b20cf29d555f7fd68ee91c244c66
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-03-16 11:37:55 +0000

    Initial commit: Project structure setup

---
+++ Cosmetica--API/app/Models/Product.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Product extends Model
+{
+    /** @use HasFactory<\Database\Factories\ProductFactory> */
+    use HasFactory;
+
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function images()
+    {
+        return $this->hasMany(ProductImage::class);
+    }
+
+    public function Orders()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+}
